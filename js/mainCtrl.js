7955298d8eee4769aebef4067525b324da365bd5
James Putnam <jonathan.putnam86@gmail.com>
2016-05-26 08:13:27 -0600

    chatroom needs Parse still

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,14 +1,28 @@
 angular.module('chatroom').controller('mainCtrl', function($scope, parseService){
   //In your controller you'll have a getParseData function and a postData function, but should be placed on $scope.
 
-  //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
+  //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to
   //your controllers $scope as messages ($scope.messages)
+  $scope.getParseData = function(){
+      parseService.getData()
+      .then(function(response){
+       $scope.messages = response;
+       console.log(response);
+      })
+    }
+    $scope.getParseData();
+
 
 
 
   //The postData function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postData method on the parseService object which will then post it to the parse backend.
+$scope.postData = function(message) {
+  parseService.postData().then(function())
 
+
+
+}
 
 
 
@@ -17,4 +31,4 @@ angular.module('chatroom').controller('mainCtrl', function($scope, parseService)
   // setInterval(function(){
   //   $scope.getParseData();
   // }, 1500)
-})
+});
